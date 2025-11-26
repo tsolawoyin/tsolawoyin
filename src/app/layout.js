8a5a49e5d5f14f771aa06cb9d022e2d0e62e7d0c
tsolawoyin <t.olawoyin@outlook.com>
2025-11-26 19:01:54 +0100

    Remove favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,6 @@
 import {Roboto, Geist, Geist_Mono, Ubuntu } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"]
